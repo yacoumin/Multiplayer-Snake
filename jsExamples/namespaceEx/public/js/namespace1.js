2f5a2f4dde9f53c5850e40f47b594e9692fd2f06
Mina Yacoup <yacoup_mina@yahoo.com>
2016-10-19 22:56:01 -0500

    adding namespace examples

---
+++ jsExamples/namespaceEx/public/js/namespace1.js
@@ -0,0 +1,21 @@
+var displayContainer = $("#display");
+var display = $("#display")[0].getContext("2d");
+var SQ_SIZE = 20;
+var width = displayContainer.width() / SQ_SIZE;
+var height = displayContainer.height() / SQ_SIZE;
+
+var socket = io.connect("/");
+
+socket.on('random', function(data){
+  $("#randContainer").html(data.rand);
+  //drawSquare(data.x,data.y,"black");
+});
+
+function drawSquare(x, y, color) {
+  display.fillStyle = color;
+
+  if (!(0 <= x && x < width && 0 <= y && y < height))
+    return;
+
+  display.fillRect(x*SQ_SIZE, y*SQ_SIZE, SQ_SIZE, SQ_SIZE);
+}
